03eb20020a5b138128aa4337ce0e2f405fe2c275
JohnsonLinnn <105207346@g.nccu.edu.tw>
2021-06-28 06:23:30 +0800

    sorting done

--- sort.js
+++ sort.js
@@ -131,3 +131,29 @@ function mergeSort (array) {
 //console.log('buble ',numbers);
 //insertionSort(nu,mbers);
 //console.log(numbers);
+
+//#1 - Sort 10 schools around your house by distance:
+//insertion
+//->small dataset
+//#2 - eBay sorts listings by the current Bid amount:
+//counting or radix 
+//-> fix integer
+//#3 - Sport scores on ESPN
+//Quick sort
+//-->space compecity
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+//Merge sort
+//-->wired 
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+//insert
+
+//#6 - Temperature Records for the past 50 years in Canada
+//radix or counting -->
+//quick
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+//Merge 
+
+//#8 - You want to teach sorting for the first time
+//bubble or selection
